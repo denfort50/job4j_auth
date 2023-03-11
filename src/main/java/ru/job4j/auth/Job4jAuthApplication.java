36b2c4d69f7fcf08eae88edcd527987cecf5d4a0
denfort50 <denfort50@yandex.ru>
2023-03-11 08:53:53 +0300

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/auth/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/auth/Job4jAuthApplication.java
@@ -1,19 +1,38 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
-public class Job4jAuthApplication {
+import javax.sql.DataSource;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Job4jAuthApplication.class, args);
+@SpringBootApplication
+public class Job4jAuthApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Job4jAuthApplication.class);
+	}
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource dataSource) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+		liquibase.setDataSource(dataSource);
+		return liquibase;
 	}
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jAuthApplication.class, args);
+	}
+
 }
